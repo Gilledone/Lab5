3543003b70d8fd517db23c991a7c76656ef3bc4f
Gilledone <migillis89@gmail.com>
2020-07-16 05:13:31 +0000

    removeemployee

--- employee.cs
+++ employee.cs
@@ -4,56 +4,61 @@ class Employee {
     //Fields
     private string firstname;
     private string lastname;
-    private string id;
+    private int id;
     private int age;
     private string address;
     private string phonenumber;
     private string title;
-    private string employeestatus;
+    private string employmentstatus;
     private double yearlysalary;
 
-/*
-    //Properties
-    public string Name{get; set;}//This is called auto implemented
-    
-    public int Age {get; set;}
+    public int Age {
       get{ return age; }
       set{
         if (value>=18)
-          age = value;
+        age = value;
         else
-          age = 18;
+      {
+        age=18;
+        Console.WriteLine("Age cannot be under 18");      
       }
     }
+  }
 
-    public double YearlySalary {get; set;}    
+    public double YearlySalary {
       get{ return yearlysalary; }
       set{
-        if (value < 1000)
-          yearlysalary = 1000;
+        if (value>=1000)
+        yearlysalary = value;
         else
-          yearlysalary = value;
+        {
+          yearlysalary=1000;
+          Console.WriteLine("Salary cannot be less than 1000");
+        }
       }
     }
 
-*/
+
+
     public Employee()//Constructor with no parameters
     {
       id=0;
-      age=0;
+      Age=0;
       firstname="unknown";
       lastname="unknown";
+      employmentstatus="inactive";
 
       Console.WriteLine("New employee created");
     }
 
-    public Employee(string employeename, string employeelast, string employeeid, int employeeage, double employeesalary)//Constructor with parameters
+    public Employee(string employeename, string employeelast, int employeeid, int employeeage, double employeesalary, string employeestatus)//Constructor with parameters
     {
       firstname = employeename;
       lastname = employeelast;
       id = employeeid;
-      age = employeeage;
+      Age = employeeage;
       yearlysalary = employeesalary;
+      employmentstatus = employeestatus;
     }
 
     public void Intro()
@@ -61,15 +66,20 @@ class Employee {
       Console.WriteLine("My name is " + firstname);
       Console.WriteLine("Last name " + lastname);
       Console.WriteLine("My id is " + id);
-      Console.WriteLine("My age is " + age);
+      Console.WriteLine("My age is " + Age);
       Console.WriteLine("My yearly salary is $" + yearlysalary);
+      Console.WriteLine("My status is " + employmentstatus);
     }
 
 
-/*
-    public void RemoveEmployee()
-    {
-      Console.WriteLine("Checking employment status");
-*/
+    public void RemoveEmployee() {
+      if (employmentstatus = active)
+      employmentstatus = active;
+      else{
+        
+      }
+    
+
     }
+    
 
